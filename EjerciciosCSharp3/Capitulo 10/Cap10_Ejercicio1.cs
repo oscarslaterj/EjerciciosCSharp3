76f72a566518185ba305fe95faea3301668407f9
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-12 14:13:55 -0400

    Agregando Ejercicio 1 Capitulo 10

---
+++ EjerciciosCSharp3/Capitulo 10/Cap10_Ejercicio1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp3.Capitulo_10
+{
+    public class Cap10_Ejercicio1
+    {
+        public class Inventario
+        {
+            public List<Producto> Productos { get; set; }
+            public int CantidadProductos { get; set; }
+            public float PrecioTotalProductos { get; set; }
+
+           
+            public void AgregarProducto(string descripcion, float precio)
+            {
+                Productos.Add(new Producto(0, descripcion, 0, precio));
+            }
+
+            public void EliminarProducto(Producto producto)
+            {
+                Productos.Remove(producto);
+            }
+
+            public class Producto
+            {
+                public int Codigo { get; set; }
+                public string Descripcion { get; set; }
+                public int CantidadExistencia { get; set; }
+                public float Precio { get; set; }
+
+                public Producto(int codigo, string descripcion, int cantidadExistencia, float precio)
+                {
+                    Codigo = codigo;
+                    Descripcion = descripcion;
+                    CantidadExistencia = cantidadExistencia;
+                    Precio = precio;
+                }
+            }
+        }
+    }
+}

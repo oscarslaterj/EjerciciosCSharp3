f0744865d3a15ad07d9975e106ed66dfdd31db5f
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-12 14:00:52 -0400

    Agregando 1er Ejercicio Capitulo 9

---
+++ EjerciciosCSharp3/Capitulo 9/Cap9_Ejercicio1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp3.Capitulo_9
+{
+    public class Cap9_Ejercicio1
+    {
+        public struct EstructuraTienda
+        { 
+            public string Precio { get; set; }
+            public string NombreProducto { get; set; }
+            public string CodigoBarras { get; set; }
+            public string Cantidad { get; set; }
+        }
+    }
+}

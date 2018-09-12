2ee471741ec5636562816e65919f7c8ae01e6f22
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-12 14:11:57 -0400

    Agregando Ejercicio 4 del capitulo 9

---
+++ EjerciciosCSharp3/Capitulo 9/Cap9_Ejercicio4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp3.Capitulo_9
+{
+    public class Cap9_Ejercicio4
+    {
+        public enum Neumaticos { Para_Coche, Para_Furgoneta, Para_4X4 };
+        public void TipoNeumatico()
+        {
+
+            Console.Clear();
+            Console.WriteLine("Tipos de Neumaticos: \n\n");
+            Console.WriteLine(Neumaticos.Para_4X4.ToString());
+            Console.WriteLine(Neumaticos.Para_Coche.ToString());
+            Console.WriteLine(Neumaticos.Para_Furgoneta.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}

662174117c95888dc71a3a7f24aef07daa324b4e
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-12 14:15:05 -0400

    Agregando ejercicio 2 Capitulo 10

---
+++ EjerciciosCSharp3/Capitulo 10/Cap1_Ejercicio2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp3.Capitulo_10
+{
+    public class Cap1_Ejercicio2
+    {
+        public class Estudiante
+        {
+            public int NumeroOrden { get; set; }
+            public string Nombres { get; set; }
+            public string Apellidos { get; set; }
+            public DateTime FechaNacimiento { get; set; }
+            public List<int> NotasMensuales { get; set; }
+
+            public Estudiante()
+            {
+
+            }
+
+            public Estudiante(int numeroOrden, string nombres, string apellidos, DateTime fechaNacimiento)
+            {
+                NumeroOrden = numeroOrden;
+                Nombres = nombres;
+                Apellidos = apellidos;
+                FechaNacimiento = fechaNacimiento;
+                NotasMensuales = new List<int>();
+            }
+
+        }
+    }
+}

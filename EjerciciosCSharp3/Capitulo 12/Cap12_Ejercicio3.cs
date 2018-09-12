ee50280794a2cbdc4ba2d8dc11244febccfce3cb
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-12 14:22:14 -0400

    Fin capitulos

---
+++ EjerciciosCSharp3/Capitulo 12/Cap12_Ejercicio3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp3.Capitulo_12
+{
+    public class Cap12_Ejercicio3
+    {
+        public void Excepcion()
+        {
+            Console.WriteLine("Excepciones de arreglos:\n1-IndexOutOfRangeException.\n3-ArgumentException.\n4-ArgumentOutOfRangeException.5-ArrayTypeMismatchException");
+        }
+    }
+}

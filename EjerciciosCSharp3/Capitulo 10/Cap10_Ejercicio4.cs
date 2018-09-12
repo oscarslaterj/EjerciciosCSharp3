381d8b70c41fc02c9b34b447171b01f1e0b98e7c
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-12 14:17:30 -0400

    Agregando Ejercicio 4 del capitulo 10

---
+++ EjerciciosCSharp3/Capitulo 10/Cap10_Ejercicio4.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp3.Capitulo_10
+{
+    public class Cap10_Ejercicio4
+    {
+        public class Poligono2
+        {
+            public int NumeroLados
+            {
+                get
+                {
+                    return NumeroLados;
+                }
+                set
+                {
+                    if (NumeroLados > 0)
+                        NumeroLados = value;
+                }
+            }
+            public int NumeroVertices
+            {
+                get
+                {
+                    return NumeroLados;
+                }
+                set
+                {
+                    if (NumeroLados > 0)
+                        NumeroLados = value;
+                }
+            }
+
+            public Poligono2()
+            {
+
+            }
+
+            public Poligono2(int numeroLados, int numeroVertices)
+            {
+                NumeroLados = numeroLados;
+                NumeroVertices = numeroVertices;
+            }
+
+        }
+    }
+}

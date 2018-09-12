a86bf06c86c82b4452809e252c55824c079553fd
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-12 14:18:32 -0400

    Agregando ejercicio 5 del capitulo 10

---
+++ EjerciciosCSharp3/Capitulo 10/Cap10_Ejercicio5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp3.Capitulo_10
+{
+    public class Cap10_Ejercicio5
+    {
+        public class Poligono3
+        {
+            public int NumeroLados { get; set; }
+            public int NumeroVertices { get; set; }
+
+            public Poligono3()
+            {
+
+            }
+
+            public Poligono3(int numeroLados, int numeroVertices)
+            {
+                NumeroLados = numeroLados;
+                NumeroVertices = numeroVertices;
+            }
+
+            public override string ToString()
+            {
+                return "Numero Lados: " + NumeroLados + " , Numero Vertices: " + NumeroVertices;
+            }
+        }
+    }
+}

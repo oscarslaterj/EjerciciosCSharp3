41405a521c095ce2ba62a6202c7ac2107fb0e45c
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-12 14:20:42 -0400

    Agregando Ejercicio 2 del capitulo 12

---
+++ EjerciciosCSharp3/Capitulo 12/Cap12_Ejercicio2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp3.Capitulo_12
+{
+    public class Cap12_Ejercicio2
+    {
+        public void Calculo()
+        {
+
+            int op = 0, n = 0, cant = 0, sum = 0, num = 0;
+
+            while (op != 3)
+            {
+                Console.Clear();
+                Console.WriteLine("Elija una opcion:\n\n1-Numero par e impar.\n2-Promedio.\n3-Salir.");
+                op = Convert.ToInt16(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Ha entrado a la funcion de numeros pares e impares.");
+                        Console.WriteLine("Ingrese un numero: ");
+                        n = Convert.ToInt32(Console.ReadLine());
+
+                        if (n % 2 == 0)
+                        {
+                            Console.WriteLine("El numero es par.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("El numero es impar");
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        Console.WriteLine("Ha salido se la funcion de numeros pares e impares");
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Ingrese la cantidad de numeros: ");
+                        cant = Convert.ToInt16(Console.ReadLine());
+
+                        for (int i = 0; i < cant; ++i)
+                        {
+                            Console.WriteLine("Ingrese el numero: " + i);
+                            num = Convert.ToInt16(Console.ReadLine());
+                            sum += num;
+                        }
+                        Console.WriteLine("El promedio es: " + sum / cant);
+                        Console.ReadKey();
+                        Console.Clear();
+                        Console.WriteLine("Ha salido de la funcion del promedio.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+    }
+}
